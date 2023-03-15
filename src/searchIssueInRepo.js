49b327a92b100b30e89d2238a2b020b2a477b77d
aldairandre <aldairandre99@gmail.com>
2023-03-15 10:03:40 +0000

    ✨ Add function to search issues

--- src/searchIssueInRepo.js
+++ src/searchIssueInRepo.js
@@ -1,34 +1,50 @@
 import octokit from "./octokit.js";
 
-const search = async (url,owner,repo) => {
+const search = async (owner,repo) => {
 
   try {
 
-    const issues = []
-
-    const { data } = await octokit.request(
-      `GET ${url}`,
+    const data = await octokit.paginate(
+      octokit.rest.issues.listForRepo,
       {
         owner : owner,
         repo : repo,
+        per_page : 100,
         headers: {
           'X-GitHub-Api-Version': '2022-11-28'
         }
       }
     );
-    
-    for (const key in data) {
-      if (Object.hasOwnProperty.call(data, key)) {
-        const element = {
-          id_issue : data[key].id,
-          issue_number : data[key].number,
-          user : data[key].user
-        };
-        issues.push(element)
-      }
-    }
 
-    return issues
+    const issues = data.filter(issue => !issue.pull_request)
+
+    const response = issues.map( issue => {
+      return {
+        id : issue.id ,
+        issue_number : issue.number,
+        user : {
+          name : issue.user.login
+        }
+      }
+    })
+    
+    return response
+
+    /* const issues = response.map(issue => {
+      
+      if(!issue.pull_request){
+        return {
+          id : issue.id ,
+          issue_number : issue.number,
+          user : {
+            name : issue.user.login
+          }
+        }
+      }
+
+    })
+
+    return issues.filter(i => i) */
 
   } catch (error) {
 
