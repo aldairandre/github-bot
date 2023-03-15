f9644ef64a9472d5b9fbd74713fb339d5b9f7cdf
aldairandre <aldairandre99@gmail.com>
2023-03-15 19:30:07 +0000

    ♻️ Refactor

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-import comment from './src/commentIssue.js';
-import searchIssues from './src/searchIssueInRepo.js';
-//import getAuth from './src/getAuth.js'
-
-/* const owner = "florinpop17"
-const repo = "app-ideas"
-const body = "Bot commented" */
+import comments from "./src/comments.js"
 
 const owner = "aldairandre"
 const repo = "github-bot"
 const body = "Bot commented"
 
-const url = "/repos/aldairandre/github-bot/issues"
-//const url2 = "/repos/florinpop17/app-ideas/issues"
+export default async function main() {
 
-const issues = await searchIssues(url,owner,repo)
-let commented = false;
+  try {
+    
+    let response = await comments(owner,repo,body)
+    
+    console.log("")
+    console.log("=============================");
+    console.log("")
+    console.log("I already did my chores.")
+    console.log("")
+    console.log("=============================")
 
-for (const issue in issues) {
-  if (Object.hasOwnProperty.call(issues, issue)) {
-    const { issue_number } = issues[issue];
+    return response
 
-    const url = `/repos/${owner}/${repo}/issues/${issue_number}/comments`
-
-    await comment(url,owner,repo,issue_number,body)
-
-    commented = true
+  } catch (error) {
+    console.log(error)  
   }
 }
 
-
-
-console.log("")
-console.log("=============================");
-console.log("")
-
-if(commented && issues.length >= 2){
-  console.log(`I just finish my task, commented in ${issues.length} issues`)
-}if (commented && issues.length === 1) {
-  console.log(`I just finish my task, commented in ${issues.length} issue`)
-} else {
-  console.log("No tasks to do")
-}
-
-console.log("")
-console.log("=============================")
+main()
