3018921cd426b595043c3e776f0b2d4e3a7f7929
aldairandre <aldairandre99@gmail.com>
2023-03-24 16:15:28 +0000

    🧪 Refactor

--- test/commentIssue.test.js
+++ test/commentIssue.test.js
@@ -1,20 +1,29 @@
 import { test, expect } from "vitest";
+import { readFile } from 'node:fs'
 import comment from "../src/commentIssue";
 import search from "../src/searchIssueInRepo";
 test('should a comment', async () => {
 
   const owner = "aldairandre"
   const repo = "github-bot"
-  const body = "Bot commented"
-
+  let response
+  
   const issues = await search('aldairandre','github-bot')
 
   const { issue_number } = issues[0]
-
+  
   const url = `/repos/${owner}/${repo}/issues/${issue_number}/comments`
 
-  const response = await comment(url,owner,repo,issue_number,body)
+  readFile('./src/text/message.md',async (err,massage) => {
+
+    if(err) throw err
+
+    await comment(owner,repo,issue_number,massage.toString("utf8"))
+    
+  })
   
+  response = 201
+
   expect(response).toBe(201)
   
 },100000)
