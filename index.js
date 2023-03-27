916fc27b8792bf060921accdb586efe62cf8e7fc
aldairandre <aldairandre99@gmail.com>
2023-03-27 17:29:37 +0100

    ✨ Feature
    Add create comment pr

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import getInputs from "./src/input/index.js"
 import comments from "./src/issues/comments.js"
+import create_comment_pr from "./src/pull_requests/create_comment.js"
 
 export default async function main() {
 
@@ -10,6 +11,7 @@ export default async function main() {
     const { owner, repo } = inputs
 
     let response = await comments(owner,repo)
+    await create_comment_pr(owner,repo)
 
     return response
 
